bf28c240e4273b894fc241fda1cfce1534b0ad0f
liesemarques <liee.m.teixeira@gmail.com>
2022-08-23 18:57:21 -0300

    Integração JS JSX

---
+++ pages/basicos/jsx2.jsx
@@ -0,0 +1,8 @@
+export default function jsx2(){
+    const Conteudo = (
+        <div>
+            <h1>JSX2</h1>
+        </div>
+    )
+    return Conteudo
+}
---
+++ pages/basicos/jsx4.jsx
@@ -0,0 +1,10 @@
+export default function jsx4(){
+
+    const Conteudo = "JSX"
+    return(
+        <div>
+            <h1>Integração JS com JSX</h1>
+            <h2>{Conteudo}</h2>
+        </div>
+    )
+}
