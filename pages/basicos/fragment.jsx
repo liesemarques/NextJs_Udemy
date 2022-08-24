953b1b313fee964b1e8096bb774fd2077079373e
liesemarques <liee.m.teixeira@gmail.com>
2022-08-23 23:55:53 -0300

    fragment

---
+++ pages/basicos/fragment.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Fragment(){
+
+    return(
+        <React.Fragment>
+            <h1>Fragmento</h1>
+            <h2>22</h2>
+        </React.Fragment>
+    )
+}
+        
